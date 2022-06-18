6d88e404b4a74645e270df59f08cd923f62ddbb9
akashurrehman <fa19-bse-112@cuilahore.edu.pk>
2022-06-18 14:26:54 +0500

    commit

--- section-1-Node-NPM/boilerplate-express-gomix/myApp.js
+++ section-1-Node-NPM/boilerplate-express-gomix/myApp.js
@@ -1,14 +1,24 @@
 let express = require('express');
 let app = express();
-
+const path=require("path");
+const dotenv=require("dotenv").config();
 console.log("Hello World")
 
 app.use('/public',express.static(path.join(__dirname, 'public/style.css')));
 
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
+  if(process.env.MESSAGE_STYLE == "uppercase")
+    {
+        res.json({
+        message: "HELLO JSON"
+        });
+    }
+    else
+    {
+        res.json({
+        message: "Hello json"
+        });
+    }
 });
 
 
